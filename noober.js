be5ea74049ad0b38568991e5c6647c7aa3c3a381
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-04-20 20:59:56 -0500

    v1.0

--- noober.js
+++ noober.js
@@ -13,5 +13,33 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  //Store the value of the contact info including first/last name and phone number
+  let passengerContactInfo = ride.passengerDetails.first + ` ` + ride.passengerDetails.last + ` - ` + ride.passengerDetails.phoneNumber
+
+  //Store the value of the pickup location
+  let pickupAddress = ride.pickupLocation.address + `, ` + ride.pickupLocation.city + `, ` + ride.pickupLocation.state + ` ` + ride.pickupLocation.zip
+
+  //Store the value of the dropoff location
+  let dropoffAddress = ride.dropoffLocation.address + `, ` + ride.dropoffLocation.city + `, ` + ride.dropoffLocation.state + ` ` + ride.dropoffLocation.zip
+
+  //Store the level of service
+  let serviceLevel
+  
+  //if the serviceLevel is specified as Purple, set the level to Purple
+  if (ride.purpleRequested) {
+    serviceLevel = `Noober Purple Passenger`
+  } 
+  //if the number of passengers is greater than 3, need to set to XL
+  else if (ride.numberOfPassengers > 3) {
+    serviceLevel = `Noober XL Passenger`
+  }
+  //all other cases will be set to X level
+  else {
+    serviceLevel = `Noober X Passenger`
+  }
+
+  //Write everything out to the console in a human-readable format
+  console.log(`${serviceLevel}: ${passengerContactInfo}. Pickup at ${pickupAddress}. Drop-off at ${dropoffAddress}.`)
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
